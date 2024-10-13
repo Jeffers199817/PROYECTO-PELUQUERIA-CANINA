8d7987ab4a06e8712e9f453844c11a0c9372c509
Jefferson Alquinga <144504631+Jeffers199817@users.noreply.github.com>
2024-10-12 23:08:00 -0500

    6 Final del sprint 4 creación de clasess Mascota y Duenio

--- src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
@@ -9,7 +9,7 @@ package com.mycompany.peluqueriacanina.logica;
  * @author JEFFERSON ALQUINGA
  */
 public class Mascota {
-    
+     
     private int num_cliente;
     private String nombre;
     private String raza;
@@ -17,4 +17,87 @@ public class Mascota {
     private String alergico;
     private String atencion_especial;
     private String observaicones;
+    
+    private Duenio unDuenio;
+
+    public Mascota() {
+    }
+
+    public Mascota(int num_cliente, String nombre, String raza, String color, String alergico, String atencion_especial, String observaicones, Duenio unDuenio) {
+        this.num_cliente = num_cliente;
+        this.nombre = nombre;
+        this.raza = raza;
+        this.color = color;
+        this.alergico = alergico;
+        this.atencion_especial = atencion_especial;
+        this.observaicones = observaicones;
+        this.unDuenio = unDuenio;
+    }
+
+    public int getNum_cliente() {
+        return num_cliente;
+    }
+
+    public void setNum_cliente(int num_cliente) {
+        this.num_cliente = num_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getAlergico() {
+        return alergico;
+    }
+
+    public void setAlergico(String alergico) {
+        this.alergico = alergico;
+    }
+
+    public String getAtencion_especial() {
+        return atencion_especial;
+    }
+
+    public void setAtencion_especial(String atencion_especial) {
+        this.atencion_especial = atencion_especial;
+    }
+
+    public String getObservaicones() {
+        return observaicones;
+    }
+
+    public void setObservaicones(String observaicones) {
+        this.observaicones = observaicones;
+    }
+
+    public Duenio getUnDuenio() {
+        return unDuenio;
+    }
+
+    public void setUnDuenio(Duenio unDuenio) {
+        this.unDuenio = unDuenio;
+    }
+    
+    
+    
 }
